4103afc3dc84935d66c21d22503cfade38a665d2
David Haisman <haisman.david@seznam.cz>
2025-04-21 12:10:15 +0200

    Created src and tb files

---
+++ src/sim_1/tb_ADT7420_driver.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 21.04.2025 00:17:47
+-- Design Name: 
+-- Module Name: tb_ADT7420_driver - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_ADT7420_driver is
+--  Port ( );
+end tb_ADT7420_driver;
+
+architecture Behavioral of tb_ADT7420_driver is
+
+begin
+
+
+end Behavioral;
