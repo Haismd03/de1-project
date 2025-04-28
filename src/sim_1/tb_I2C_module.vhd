c48d97fad13ec71c772856851697af0c14cb4f5a
adam.kerka <adam.kerka@gmail.com>
2025-04-28 12:13:54 +0200

    Master frames
    Posílání adresy, registru a ACK check (frame 1 a 2). Logika 0/1.

---
+++ src/sim_1/tb_I2C_module.vhd
@@ -0,0 +1,120 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_I2C_module is
+end tb_I2C_module;
+
+architecture tb of tb_I2C_module is
+
+    component I2C_module
+        port (
+            address   : in std_logic_vector(6 downto 0);
+            reg       : in std_logic_vector(7 downto 0);
+            rw        : in std_logic;
+            num_bytes : in integer;
+            data      : in std_logic_vector(7 downto 0);
+            clk       : in std_logic;
+            rst       : in std_logic;
+            SDA       : inout std_logic;
+            SCL       : out std_logic;
+            response  : out std_logic_vector(7 downto 0);
+            done      : out std_logic
+        );
+    end component;
+
+    -- Signály pro propojení
+    signal address   : std_logic_vector(6 downto 0) := (others => '0');
+    signal reg       : std_logic_vector(7 downto 0) := (others => '0');
+    signal rw        : std_logic;
+    signal num_bytes : integer;
+    signal data      : std_logic_vector(7 downto 0) := (others => '0');
+    signal clk       : std_logic := '0';
+    signal rst       : std_logic;
+    signal SDA       : std_logic := 'Z';
+    signal SCL       : std_logic;
+    signal response  : std_logic_vector(7 downto 0);
+    signal done      : std_logic;
+
+    constant TbPeriod : time := 250 ns;
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    -- Propojení DUT
+    dut : I2C_module
+        port map (
+            address   => address,
+            reg       => reg,
+            rw        => rw,
+            num_bytes => num_bytes,
+            data      => data,
+            clk       => clk,
+            rst       => rst,
+            SDA       => SDA,
+            SCL       => SCL,
+            response  => response,
+            done      => done
+        );
+
+    -- Generování hodin
+    clk <= not clk after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- Stimuli process
+    stimuli : process
+    begin
+        -- Inicializace
+        address <= (others => '0');
+        reg <= (others => '0');
+        rw <= '0';
+        num_bytes <= 0;
+        rst <= '0';
+        
+        wait for 100 ns;
+         
+        -- Reset
+        -- rst <= '1'; -- resetování modulu pokud bude potřeba
+        -- wait for 100 ns;
+        -- rst <= '0';
+        -- wait for 100 ns;
+
+        -- ADT7420 driver
+        address <= "1001011"; -- 0x4B
+        reg <= "00000000";    -- 0x00
+        num_bytes <= 2;
+        
+        -- nepoužívá se prozatim
+        --data <= (others => '0'); 
+        --response <= (others => '0');
+        --done <= '0';
+
+        -- Počkat než začne odesílání adresy
+        wait for 2400 ns;
+
+        -- První ACK po adrese
+        SDA <= '0'; -- Slave drží sběrnici na 0
+        wait for TbPeriod;
+        SDA <= 'Z'; -- Uvolnění sběrnice
+
+        -- Počkat než začne odesílání registru
+        wait for 2250 ns;
+
+        -- Druhý ACK po registru
+        SDA <= '0';
+        wait for TbPeriod;
+        SDA <= 'Z';
+
+        -- Počkáme na konec přenosu
+        wait for 20*TbPeriod;
+
+        -- Ukončení simulace
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Konfigurace (většinou není třeba upravovat)
+configuration cfg_tb_I2C_module of tb_I2C_module is
+    for tb
+    end for;
+end cfg_tb_I2C_module;
--- src/sources_1/I2C_module.vhd
+++ src/sources_1/I2C_module.vhd
@@ -32,16 +32,173 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity I2C_module is
-    Port ( address : in STD_LOGIC_VECTOR (7 downto 0);
+
+    -- generics?
+
+    Port ( address : in STD_LOGIC_VECTOR (6 downto 0); -- 0x4B => 0b1001011
+           reg : in STD_LOGIC_VECTOR (7 downto 0); -- 0x00 => 0b00000000
+           rw : in STD_LOGIC;
+           num_bytes : in integer range 0 to 2;
            data : in STD_LOGIC_VECTOR (7 downto 0);
-           SDA : out STD_LOGIC_VECTOR (7 downto 0);
+           clk : in STD_LOGIC; -- 400 kHz
+           rst : in STD_LOGIC;
+           SDA : inout  STD_LOGIC;
            SCL : out STD_LOGIC;
-           response : out STD_LOGIC_VECTOR (7 downto 0));
+           response : out STD_LOGIC_VECTOR (7 downto 0);
+           done : out STD_LOGIC);
 end I2C_module;
 
 architecture Behavioral of I2C_module is
 
+    -- reset constansts
+--    constant RESET_LOGIC : STD_LOGIC := '0';
+--    constant RESET_LOGIC_VECTOR : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+--    constant RESET_INT : integer := 0;
+    
+    -- read write logic constants
+    constant WRITE : std_logic := '0';
+    constant READ  : std_logic := '1';
+
+    type  state_type is (WAIT_FOR_DATA, START_CONDITION, SEND_ADDRESS, SEND_REGISTER, STOP_CONDITION, READ_DATA, CHECK_ACK, NACK);
+    signal state : state_type := WAIT_FOR_DATA;
+    signal next_state : state_type;
+    
+    signal bit_cnt : integer range 0 to 7 := 0;
+    signal frame_1 : STD_LOGIC_VECTOR (7 downto 0);
+    signal frame_2 : STD_LOGIC_VECTOR (7 downto 0);
+    signal i : integer := 1; -- iteration
+    signal ack_wait : std_logic := '0';
+    
 begin
+--    p_SCL_driver : process (clk, state)
+--    begin
+--        if (state = SEND_ADDRESS or state = SEND_REGISTER or state = CHECK_ACK or state = STOP_CONDITION) then
+--            if (clk = '1') then
+--                SCL <= '1';
+--            elsif (clk = '0') then
+--                SCL <= '0';
+--            else
+--                SCL <= '0'; -- Default fallback pro nevalidní clk
+--            end if;
+--        else
+--            SCL <= '1';
+--        end if;
+--    end process;
 
+    p_I2C_module: process (clk)
+    begin
+        if (rst = '1') then                            
+--          response <= (others => RESET_LOGIC);
+            frame_1 <= (others => '0');
+            frame_2 <= (others => '0');
+            
+            bit_cnt <= 0;
+            done <= '0';            
+            -- next state
+            state <= WAIT_FOR_DATA;
+        end if;      
+        
+        case state is
+            when WAIT_FOR_DATA =>
+                SDA <= '1';
+                SCL <= '1';
+                
+                if (rising_edge(clk)) then
+                    if (num_bytes /= 0) then                                         
+                        frame_1 <= address & WRITE;
+                        frame_2 <= reg;
+                        -- next state
+                        state <= START_CONDITION;
+                    end if;
+                end if;
+                
+            when START_CONDITION =>
+                -- SDA 1 -> 0, SCL = 1
+                if (rising_edge(clk)) then
+                    SDA <= '0';
+                    -- next state                   
+                    state <= SEND_ADDRESS;
+                end if;
+                                 
+            when SEND_ADDRESS =>
+                if (falling_edge(clk)) then
+                    SCL <= clk; -- 400 kHz
+                    SDA <= frame_1(7 - bit_cnt);                                                                      
+                    
+                    if (bit_cnt = 7) then
+                        bit_cnt <= 0; 
+                        -- next state                    
+                        state <= CHECK_ACK;
+                        next_state <= SEND_REGISTER;
+                    else
+                        bit_cnt <= bit_cnt + 1;                     
+                    end if;
+                else
+                    SCL <= clk; -- rising_edge
+                end if;
+                             
+            when CHECK_ACK =>
+                SCL <= clk;
+                
+--                if (falling_edge(clk)) then
+--                    SDA <= 'Z';
+--                    if (SDA = '0') then
+--                        -- next state
+--                        state <= next_state;
+--                    else
+--                        -- ADT7420s not responding
+--                        report "NACK";
+--                        -- state <= NACK
+--                    end if;
+--                end if;
+                
+                if (falling_edge(clk)) then
+                    if ack_wait = '0' then
+                        SDA <= 'Z';
+                        ack_wait <= '1'; -- wait for another rising_edge
+                    end if;
 
+                elsif (rising_edge(clk)) then
+                    if ack_wait = '1' then
+                        if SDA = '0' then
+                            -- ACK
+                            state <= next_state;  
+                        else
+                            -- ADT7420s not responding
+                            report "NACK";
+                            -- state <= NACK;
+                        end if;
+                        ack_wait <= '0'; -- reset
+                    end if;
+                end if;
+                                
+            when SEND_REGISTER =>
+                if (falling_edge(clk)) then
+                    SDA <= frame_2(7 - bit_cnt);
+                    SCL <= clk;
+                    
+                    if (bit_cnt = 7) then
+                        -- next state
+                        state <= CHECK_ACK;
+                        next_state <= STOP_CONDITION;
+                    else
+                        bit_cnt <= bit_cnt + 1;
+                    end if;
+                else
+                    SCL <= clk;
+                end if;
+                
+            when STOP_CONDITION =>
+                if (falling_edge(clk)) then
+                    -- SDA 0 -> 1, SCL = 1
+                    SCL <= '1';
+                    SDA <= '1';
+                    state <= READ_DATA;
+                end if;
+                
+            when others =>
+                state <= WAIT_FOR_DATA; 
+                
+        end case;
+    end process p_I2C_module;
 end Behavioral;
