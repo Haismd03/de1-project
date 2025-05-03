f5bb86751b7735dc6f81a2a7bcc23be9dd5c7844
David Haisman <haisman.david@seznam.cz>
2025-05-03 18:24:00 +0200

    Working and synthesizable I2C_driver

--- src/sources_1/I2C_driver.vhd
+++ src/sources_1/I2C_driver.vhd
@@ -49,19 +49,46 @@ entity I2C_driver is
     );
 end I2C_driver;
 
-architecture Behavioral of I2C_driver is
-    signal frame_read : std_logic_vector(15 downto 0);
-    
+architecture Behavioral of I2C_driver is  
     signal SDA_drive : std_logic := 'Z';
     signal SCL_drive : std_logic := 'Z';
+    signal disable_auto_SCL : std_logic := '1';
     
     signal counter : integer := 0;
+    signal read_counter : integer := 0;
     
-    type  state_type is (RESET, IDLE, START, SEND_ADDRESS, CHECK_ACK, CHECK_ACK_2, SEND_REGISTER, ERROR);
+    type state_type is (RESET, IDLE, START, ERROR, STOP,
+                        SEND_ADDRESS_W, SEND_ADDRESS_R, SEND_REGISTER, SEND_ACK, SEND_NACK,
+                        CHECK_ACK, READ_DATA,
+                        SEND_TO_MASTER, END_STATE);
+    type state_array is array(integer range <>) of state_type;
+
+    constant state_sequence : state_array := (
+        RESET,
+        IDLE,
+        START,
+        SEND_ADDRESS_W,
+        CHECK_ACK,
+        SEND_REGISTER,
+        CHECK_ACK,
+        START,
+        SEND_ADDRESS_R,
+        CHECK_ACK,
+        READ_DATA,
+        SEND_ACK,
+        READ_DATA,
+        SEND_NACK,
+        STOP,
+        SEND_TO_MASTER, 
+        END_STATE -- last state -> reset when done
+    );
+
     signal state : state_type := RESET;
+    signal state_idx : integer := state_sequence'low;
 begin
     
     rising_process : process(clk)
+        variable read_size : integer := 0;
     begin
         if (rising_edge(clk)) then
         
@@ -74,37 +101,61 @@ begin
                     SDA_drive <= 'Z';
                     SCL_drive <= 'Z';  
                                
-                    response <= (others => '0');
-                    frame_read <= (others => '0');
-                    
-                    done <= '0';   
+                    done <= '0';
                     bit_error <= '0';
+                    response <= (others => '0');
+                    
+                    counter <= 0;
+                    read_counter <= 0;
                              
                     -- next state
                     if (rst /= '1') then
-                        --state <= IDLE;
-                        state <= IDLE;
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
                     end if;
                     
                  when IDLE =>
                     if (num_bytes /= 0) then
-                        state <= START;
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
                     end if;   
                     
                 when START =>
-                    state <= SEND_ADDRESS;
+                    if (counter = 0) then
+                        disable_auto_SCL <= '1';
+                        counter <= counter + 1;
+                    else
+                        counter <= 0;
+                        
+                        disable_auto_SCL <= '0';
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
+                    end if;
                     
-                when SEND_ADDRESS =>
+                when SEND_ADDRESS_W =>
                     if (counter < 7) then
                         counter <= counter + 1;
                     else
-                        state <= CHECK_ACK;
                         counter <= 0;
+                    
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;  
+                    end if; 
+                    
+                when SEND_ADDRESS_R =>
+                    if (counter < 7) then
+                        counter <= counter + 1;
+                    else
+                        counter <= 0;
+                        
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
                     end if; 
                     
                 when CHECK_ACK =>
-                    if (SDA = 'H') then
-                        state <= SEND_REGISTER;
+                    if (SDA = '0') then
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
                     else
                         state <= ERROR;
                     end if; 
@@ -113,16 +164,65 @@ begin
                     if (counter < 7) then
                         counter <= counter + 1;
                     else
-                        state <= CHECK_ACK_2;
                         counter <= 0;
-                    end if;  
+                        
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
+                    end if; 
                     
-                when CHECK_ACK_2 =>
+                when READ_DATA =>
+                    read_size := 8*(num_bytes - read_counter) - 1;
                     if (SDA = 'H') then
-                        state <= START;
+                        response(read_size - counter) <= '1';
                     else
-                        state <= ERROR;
-                    end if;               
+                        response(read_size - counter) <= '0';
+                    end if;
+                    
+                    if (counter < 7) then
+                        counter <= counter + 1;
+                    else
+                        counter <= 0;
+                        read_counter <= read_counter + 1;
+                        
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
+                    end if; 
+                    
+                when SEND_ACK =>
+                    state <= state_sequence(state_idx);
+                    state_idx <= state_idx + 1;
+                    
+                WHEN SEND_NACK =>
+                    state <= state_sequence(state_idx);
+                    state_idx <= state_idx + 1;   
+                 
+                WHEN STOP =>
+                    if (counter = 0) then
+                        disable_auto_SCL <= '1';
+                        counter <= counter + 1;
+                    else
+                        counter <= 0;
+                        
+                        disable_auto_SCL <= '1';
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
+                    end if;
+                    
+                WHEN SEND_TO_MASTER =>
+                    done <= '1';
+                    if (done_master_read = '1') then
+                        state <= state_sequence(state_idx);
+                        state_idx <= state_idx + 1;
+                    end if;
+                    
+                WHEN END_STATE =>
+                    state <= RESET;
+                    state_idx <= state_sequence'low; 
+                    
+                when ERROR =>
+                    bit_error <= '1';
+                    state <= RESET;
+                    state_idx <= state_sequence'low;                 
                     
                 when others =>
                     -- do nothing
@@ -135,16 +235,55 @@ begin
     begin
         if (falling_edge(clk)) then
             case state is
-            
                 when START =>
-                    SDA_drive <= '0';
+                    if (counter = 0) then
+                        SDA_drive <= 'Z';
+                    elsif (counter = 1) then
+                        SDA_drive <= '0';
+                    end if;
                         
-                when SEND_ADDRESS =>
-                    SDA_drive <= 'Z';   -- replace with actual value
+                when SEND_ADDRESS_W =>
+                    if ((7 - counter) > 0) then
+                        if (address(6 - counter) = '1') then    
+                            SDA_drive <= 'Z';
+                        else
+                            SDA_drive <= '0';
+                        end if;
+                    else
+                        SDA_drive <= '0';
+                    end if;
+                    
+                when SEND_ADDRESS_R =>
+                    if ((7 - counter) > 0) then
+                        if (address(6 - counter) = '1') then    
+                            SDA_drive <= 'Z';
+                        else
+                            SDA_drive <= '0';
+                        end if;
+                    else
+                        SDA_drive <= 'Z';
+                    end if;
                     
                 when SEND_REGISTER =>
-                    SDA_drive <= 'Z';   -- replace with actual value
+                    if (reg(7 - counter) = '1') then    
+                        SDA_drive <= 'Z';
+                    else
+                        SDA_drive <= '0';
+                    end if;
                     
+                when SEND_ACK =>
+                    SDA_drive <= '0';
+                    
+                when SEND_NACK =>
+                    SDA_drive <= 'Z';
+                    
+                when STOP =>
+                    if (counter = 0) then
+                        SDA_drive <= '0';
+                    elsif (counter = 1) then
+                        SDA_drive <= 'Z';
+                    end if;   
+                     
                 when others =>
                     -- do nothing
                     SDA_drive <= 'Z'; 
@@ -154,7 +293,7 @@ begin
     
     p_SCL_driver : process (clk)
     begin
-        if (state /= START and state /= IDLE and state /= RESET) then
+        if (disable_auto_SCL = '0') then
             if (clk = '1') then
                 SCL <= 'Z';
             elsif (clk = '0') then
